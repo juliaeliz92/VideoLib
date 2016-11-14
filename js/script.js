a0ba5f56ef99632192f5f2229ebaa72d423ab9b2
juliaeliz92 <juliapaulmyla@yahoo.com>
2016-11-14 09:29:46 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+$(function() {
+	$('#infoBtn').click(function(e){
+		e.preventDefault();
+       $('#logAlert').slideDown();
+     });
+	$('#infoBtn2').click(function(e){
+		e.preventDefault();
+       $('#logAlert').slideDown();
+     });
+});
+
+
